867094a3e357f5e85ba000647a689bc968683558
Mario Domenech Goulart <mario@parenteses.org>
2020-01-28 19:11:04 +0100

    Fix build for CHICKEN 4
    CHICKEN 4 needs `scheme' for `cond-expand'.

--- srfi-64.scm
+++ srfi-64.scm
@@ -1,8 +1,9 @@
 (module srfi-64 ()
+  (import scheme)
   (cond-expand
     (chicken-4
-     (import chicken scheme)
+     (import chicken)
      (use numbers))
     (chicken-5
-     (import scheme (chicken base) (chicken module))))
+     (import (chicken base) (chicken module))))
   (include "srfi-64/srfi-64-port.scm"))
